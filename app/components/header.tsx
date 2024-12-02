f550c4982806cc1a8e53fc9093591663a1c0276c
Paul Mourer <paul.mourer@gmail.com>
2024-12-02 08:38:35 -0700

    Setup, layout, fonts, colors, header experimenting

---
+++ app/components/header.tsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+  return (
+    <header className="sticky top-0 inset-x-0 py-2 bg-bg-1">
+      <nav className="container">
+        <h1 className="font-mono font-bold text-fg-1 text-lg">
+          visitor@bypaul.dev$ <span>_</span>
+        </h1>
+      </nav>
+    </header>
+  );
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,46 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    colors: {
+      bg: {
+        0: "#fbf3db",
+        1: "#ece3cc",
+        2: "#d5cdb6",
+      },
+      dim: {
+        0: "#909995",
+      },
+      fg: {
+        0: "#53676d",
+        1: "#3a4d53",
+      },
+      red: "#d2212d",
+      "red-br": "#cc1729",
+      green: "#489100",
+      "green-br": "#428b00",
+      yellow: "#ad8900",
+      "yellow-br": "#a78300",
+      blue: "#0072d4",
+      "blue-br": "#006dce",
+      magenta: "#ca4898",
+      "magenta-br": "#c44392",
+      cyan: "#009c8f",
+      "cyan-br": "#00978a",
+      orange: "#c25d1e",
+      "orange-br": "#bc5819",
+      violet: "#8762c6",
+      "violet-br": "#825dc0",
+    },
+    fontFamily: {
+      sans: "var(--font-sans)",
+      mono: "var(--font-mono)",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "1rem",
+        md: "2rem",
       },
     },
   },
